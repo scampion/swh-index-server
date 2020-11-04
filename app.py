5c5a3bb95345286d65317e8510742aa0d4848bd9
Sebastien Campion <sebastien.campion@gmail.com>
2020-11-04 23:03:12 +0000

    fix

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from flask import Flask, abort, Response
 SHA1_SIZE_IN_BYTES = 20
 SORTED_INDEX_FILEPATH = "data.bin"
 
-log.basicConfig(level=log.INFO)
+log.basicConfig(level=log.DEBUG)
 app = Flask(__name__)
 mem_bytes = os.sysconf('SC_PAGE_SIZE') * os.sysconf('SC_PHYS_PAGES')
 index_memory_size = max(1024, mem_bytes - 0.5 * (1024. ** 3) * 80.0 / 100)
@@ -21,44 +21,47 @@ log.info("sorted index size %s", os.stat(SORTED_INDEX_FILEPATH).st_size)
 log.info("sample index interval : %d", sample_index_interval)
 assert index_memory_size < os.stat(SORTED_INDEX_FILEPATH).st_size, "Binary file fit in memory, exit"
 
+def b2a(s):
+    return binascii.hexlify(s).decode("ascii")[:10] + "."
 
 idx = b''
 with open(SORTED_INDEX_FILEPATH, "rb") as f:
     sig = f.read(SHA1_SIZE_IN_BYTES)
     i = 0
     while sig:
-        f.seek(f.tell() + sample_index_interval)
+        f.seek(f.tell() + sample_index_interval * SHA1_SIZE_IN_BYTES)
         sig = f.read(SHA1_SIZE_IN_BYTES)
+        #print(b2a(sig))
         idx += sig
         progress = f.tell() / os.stat(SORTED_INDEX_FILEPATH).st_size * 100.0
         print("Indexation in progress : %2d%%" % progress, end='\r')
 
-def b2a(s):
-    return binascii.hexlify(s).decode("ascii")[:10] + "."
 
 def get_file_position(q):
     length = len(idx) / SHA1_SIZE_IN_BYTES
-    size = floor(length)
-    offset = ceil(size / 2) * SHA1_SIZE_IN_BYTES
+    size = ceil(length / 2)
+    offset = size * SHA1_SIZE_IN_BYTES
     sig = idx[offset:offset + SHA1_SIZE_IN_BYTES]
     while q != sig and size > 1:
-        log.debug("get file position for query %s : current sig %s position %10s : size %s", b2a(q), b2a(sig), offset, size)
+        log.debug("get file position for query %s : current sig %s position %10s : size %s", b2a(q), b2a(sig), offset / SHA1_SIZE_IN_BYTES, size)
         size = ceil(size / 2.)
         if q > sig:
             offset += size * SHA1_SIZE_IN_BYTES
         if q < sig:
             offset -= size * SHA1_SIZE_IN_BYTES
         sig = idx[offset:offset + SHA1_SIZE_IN_BYTES]
-    log.debug("result for file position for query %s : current position %10s : size %s : %s", b2a(q), offset, size, q != sig)
-    return offset * sample_index_interval
+    log.debug("result for file position for query %s : current position %10s : size %s : %s", b2a(q), offset / SHA1_SIZE_IN_BYTES, size, q != sig)
+    return int(offset / SHA1_SIZE_IN_BYTES)
+
 
 def is_present(q):
-    offset = get_file_position(q)
+    offset = get_file_position(q) * sample_index_interval * SHA1_SIZE_IN_BYTES
+    print("offset %s" % offset)
     with open(SORTED_INDEX_FILEPATH, "rb") as f:
         size = sample_index_interval * 2
         f.seek(offset)
         sig = f.read(SHA1_SIZE_IN_BYTES)
-        log.debug("query : %s", b2a(q))
+        log.debug("query : %s / current %s", b2a(q), b2a(sig))
         while q != sig and size > 1:
             log.debug("search %s : current sig %s : position %10s : size %s", b2a(q), b2a(sig), offset, size)
             size = ceil(size / 2.)
